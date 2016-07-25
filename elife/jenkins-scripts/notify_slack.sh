8baffd9281ef5bc46c8f5479b94f229155b673c6
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-25 14:03:46 +0100

    URL of new Alfred machine

--- elife/jenkins-scripts/notify_slack.sh
+++ elife/jenkins-scripts/notify_slack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-curl -v -d "{\"text\":\"$1\", \"username\":\"alfred\", \"icon_url\":\"http://ci.alfred.elifesciences.org/favicon.ico\"}" $SLACK_CHANNEL_HOOK
+curl -v -d "{\"text\":\"$1\", \"username\":\"alfred\", \"icon_url\":\"http://ci--alfred.elifesciences.org/favicon.ico\"}" $SLACK_CHANNEL_HOOK
 
