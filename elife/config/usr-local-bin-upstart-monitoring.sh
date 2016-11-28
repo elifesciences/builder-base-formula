16db0dbce7cadc5ccaa4614f3dfab17ea96b4074
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-28 16:43:32 +0000

    Clear error messages in upstart-monitoring.sh

--- elife/config/usr-local-bin-upstart-monitoring.sh
+++ elife/config/usr-local-bin-upstart-monitoring.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -eu
 
+if [ "$#" -ne 1 ]
+then
+    echo "Usage: $0 process-name-or-part-of-it"
+    exit 1
+fi
+
 to_match=$1
 log_file=/var/log/upstart-monitoring.log
 set -o pipefail
 initctl list | grep $to_match | tee -a $log_file | grep /running || {
-    echo "Error in upstart-monitoring: ${status}"
-    logger "Upstart job is stopped: $to_match ($log_file on $(hostname))"
-    exit $status
+    echo "Error in upstart-monitoring: no job matching ${to_match} is running"
+    logger "Upstart job matching $to_match is stopped ($log_file on $(hostname))"
+    exit 2
 }
 
