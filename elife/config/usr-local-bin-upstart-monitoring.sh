324ecde45d0846f44a517a7ad104b37b00fa4e57
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-29 11:43:30 +0000

    Exactly matching the process name

--- elife/config/usr-local-bin-upstart-monitoring.sh
+++ elife/config/usr-local-bin-upstart-monitoring.sh
@@ -10,7 +10,7 @@ fi
 to_match=$1
 log_file=/var/log/upstart-monitoring.log
 set -o pipefail
-initctl list | grep $to_match | tee -a $log_file | grep /running || {
+initctl list | grep "^$to_match " | tee -a $log_file | grep /running || {
     echo "Error in upstart-monitoring: no job matching ${to_match} is running"
     logger "Upstart job matching $to_match is stopped ($log_file on $(hostname))"
     exit 2
