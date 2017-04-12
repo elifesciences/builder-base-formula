843b2ab49cca3494b49284d7e5d791a1e04a7949
Luke Skibinski <l.skibinski@elifesciences.org>
2017-04-12 17:06:46 +0930

    added a script that adds some permissions for the rds_superuser role

---
+++ elife/scripts/rds-perms.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# it's a little backwards, but because the rds_superuser doesn't get permissions
+# on new objects, it has to be granted them. 
+set -ex
+
+{% if salt['elife.cfg']('cfn.outputs.RDSHost') %} 
+
+host={{ salt['elife.cfg']('cfn.outputs.RDSHost') }}
+port={{ salt['elife.cfg']('cfn.outputs.RDSPort') }}
+db={{ salt['elife.cfg']('project.rds_dbname') }}
+pass={{ pass }}
+user={{ user }}
+
+PGPASSWORD=$pass psql -U $user -h $host -p $port $db -c "
+-- possibly redundant
+GRANT ALL ON DATABASE $db TO rds_superuser;
+-- the important bit
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO rds_superuser;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO rds_superuser;
+-- ensure all future objects can be read
+ALTER DEFAULT PRIVILEGES GRANT ALL ON TABLES TO rds_superuser;"
+
+echo "rds_superuser permissions set"
+
+{% else %}
+
+echo "no rds permissions to set"
+
+{% endif %}
