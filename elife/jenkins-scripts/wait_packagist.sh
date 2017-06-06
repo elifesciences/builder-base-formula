16b224ae50411c5a7b505605409e74898a13211c
Giorgio Sironi <g.sironi@elifesciences.org>
2017-06-06 09:59:51 +0100

    Script for waiting for a revision to appear on Packagist

---
+++ elife/jenkins-scripts/wait_packagist.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: wait_packagist.sh PACKAGE_NAME REVISION"
+    echo "Example: wait_packagist.sh elife/patterns c5e88e0422c4933a9763dd9967be4f7d63bc9cb2"
+    exit 1
+fi
+
+package="$1"
+revision="$2"
+polling=5
+
+while : ; do
+    newest_available_revision=$(curl -v https://packagist.org/p/elife/patterns.json | jq -r ".packages[\"$package\"][\"dev-master\"].source.reference")
+    echo "Found revision $newest_available_revision"
+    if [ "$revision" != "$newest_available_revision" ]; then
+        echo "Continue waiting..."
+        sleep "$polling"
+    else
+        echo "Stop waiting."
+        break
+    fi
+done
