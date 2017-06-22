a3b33f9bcdb4a8389bbdc64f353d942ff327f166
Giorgio Sironi <g.sironi@elifesciences.org>
2017-06-22 10:25:16 +0100

    Added new_relic_apdex.sh script

--- elife/jenkins-scripts/new_relic_status.sh
+++ elife/jenkins-scripts/new_relic_status.sh
@@ -4,6 +4,7 @@ set -e
 if [ "$#" -ne 1 ]; then
     echo "Usage: new_relic_status.sh APPLICATION_ID"
     echo "Example: new_relic_status.sh 29775807"
+    echo "Returns the status color of APM/Browser monitoring. Values: red, orange, green, unknown"
     exit 1
 fi
 
