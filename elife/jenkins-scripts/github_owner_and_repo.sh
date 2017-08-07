2b8270cf976e2505d00d85e576e3de8f306d718e
Giorgio Sironi <g.sironi@elifesciences.org>
2017-08-07 15:49:25 +0100

    Robust version of github_owner_and_repo.sh

--- elife/jenkins-scripts/github_owner_and_repo.sh
+++ elife/jenkins-scripts/github_owner_and_repo.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
+# sample output:
+# elifesciences/pattern-library
 
-git remote -v 2>&1 | grep '(fetch)' | grep "^origin " | sed -e 's/.*github.com:\(.*\)\(.git\)\{0,1\} .*/\1/g'
+git remote -v 2>&1 \
+    | grep '(fetch)' \
+    | grep -P "^origin\t" \ # -P for recognizing the tab character
+    | sed -e 's/.*github.com:\(.*\)\(.git\)\{0,1\} .*/\1/g'
