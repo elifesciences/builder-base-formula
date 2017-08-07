6c21edb79e2662c4d9ddd4752cb50c5cd7f994c6
Giorgio Sironi <g.sironi@elifesciences.org>
2017-08-07 15:43:11 +0100

    More precise grep on origin

--- elife/jenkins-scripts/github_owner_and_repo.sh
+++ elife/jenkins-scripts/github_owner_and_repo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-git remote -v 2>&1 | grep fetch | grep origin | sed -e 's/.*github.com:\(.*\)\(.git\)\{0,1\} .*/\1/g'
+git remote -v 2>&1 | grep '(fetch)' | grep "^origin " | sed -e 's/.*github.com:\(.*\)\(.git\)\{0,1\} .*/\1/g'
