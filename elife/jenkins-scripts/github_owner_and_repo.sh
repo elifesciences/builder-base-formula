5a42eed1f80bcc9d53ad947bd139bef4705fb19d
Giorgio Sironi <g.sironi@elifesciences.org>
2017-08-07 15:59:14 +0100

    Removed comment breaking line and genericized first separator

--- elife/jenkins-scripts/github_owner_and_repo.sh
+++ elife/jenkins-scripts/github_owner_and_repo.sh
@@ -3,7 +3,8 @@ set -e
 # sample output:
 # elifesciences/pattern-library
 
+# `grep -P` for recognizing the tab character
 git remote -v 2>&1 \
     | grep '(fetch)' \
-    | grep -P "^origin\t" \ # -P for recognizing the tab character
-    | sed -e 's/.*github.com:\(.*\)\(.git\)\{0,1\} .*/\1/g'
+    | grep -P "^origin\t" \
+    | sed -e 's/.*github.com[:/]\(.*\)\(.git\)\{0,1\} .*/\1/g'
