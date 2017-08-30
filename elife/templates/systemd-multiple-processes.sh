62d22b30f8067549a927a234a83a65b5182c8da7
Luke Skibinski <l.skibinski@elifesciences.org>
2017-08-30 09:58:38 +0930

    fixed indentation in systemd-n-processes.sh script

--- elife/templates/systemd-multiple-processes.sh
+++ elife/templates/systemd-multiple-processes.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 NUMBER={{ number }}
 timeout=120
-    echo "--------"
-    echo "Current status of $NUMBER {{ process }} processes"
-    echo "Now is" $(date -Iseconds)
-    for i in `seq 1 $NUMBER`
-    do
-        systemctl status {{ process }}@$i -n 0 || true
-    done
+echo "--------"
+echo "Current status of $NUMBER {{ process }} processes"
+echo "Now is" $(date -Iseconds)
+for i in `seq 1 $NUMBER`
+do
+    systemctl status {{ process }}@$i -n 0 || true
+done
 
-    echo "Stopping $NUMBER {{ process }} processes"
-    echo "Now is" $(date -Iseconds)
-    for i in `seq 1 $NUMBER`
-    do
-        (systemctl stop {{ process }}@$i &) || true
-    done
+echo "Stopping $NUMBER {{ process }} processes"
+echo "Now is" $(date -Iseconds)
+for i in `seq 1 $NUMBER`
+do
+    (systemctl stop {{ process }}@$i &) || true
+done
 
-    for i in `seq 1 $NUMBER`
+for i in `seq 1 $NUMBER`
+do
+    echo "Waiting for {{ process }} $i to stop"
+    echo "Now is" $(date -Iseconds)
+    counter=0
+    while true
     do
-        echo "Waiting for {{ process }} $i to stop"
-        echo "Now is" $(date -Iseconds)
-        counter=0
-        while true
-        do
-            if [ "$counter" -gt "$timeout" ]
-            then
-                echo "It shouldn't take more than $timeout seconds to kill all the {{ process }} processes"
-                exit 1
-            fi
-            systemctl status {{ process }}@$i -n 0 | grep "Active: inactive" && break
-            sleep 1
-            counter=$((counter + 1))
-        done
+        if [ "$counter" -gt "$timeout" ]
+        then
+            echo "It shouldn't take more than $timeout seconds to kill all the {{ process }} processes"
+            exit 1
+        fi
+        systemctl status {{ process }}@$i -n 0 | grep "Active: inactive" && break
+        sleep 1
+        counter=$((counter + 1))
     done
-    echo "Stopped all {{ process }} processes"
-    echo "Starting $NUMBER {{ process }} processes"
-    for i in `seq 1 $NUMBER`
-    do
-        systemctl start {{ process }}@$i
-    done
-    echo "Started $NUMBER {{ process }} processes"
+done
+echo "Stopped all {{ process }} processes"
+echo "Starting $NUMBER {{ process }} processes"
+for i in `seq 1 $NUMBER`
+do
+    systemctl start {{ process }}@$i
+done
+echo "Started $NUMBER {{ process }} processes"
