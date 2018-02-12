2d6abdc0b8811cb8e22f0d99ac7d8f79eb1af936
Luke Skibinski <l.skibinski@elifesciences.org>
2018-02-12 17:16:14 +1030

    psql permissions script now sets flag

--- elife/scripts/postgresql-appdb-perms-fresh.sh
+++ elife/scripts/postgresql-appdb-perms-fresh.sh
@@ -19,4 +19,7 @@ GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $rootuser;
 -- ensure all future objects can be read
 ALTER DEFAULT PRIVILEGES GRANT ALL ON TABLES TO $rootuser;"
 
+# it doesn't hurt to run this script constantly, but once should be enough
+touch /root/db-permissions-set.flag
+
 echo "permissions set"
