5e10cb60eb32c0fe87adf48fed461acc68503860
Giorgio Sironi <g.sironi@elifesciences.org>
2018-06-05 16:32:33 +0100

    Avoid overwriting PATH environment variable

--- elife/scripts/create-swap.sh
+++ elife/scripts/create-swap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PATH="${1:-/var/swap.1}"
-MB="${2:-1024}"
+swap_path="${1:-/var/swap.1}"
+mb="${2:-1024}"
 
 if free | awk '/^Swap:/ {exit !$2}'; then
     echo "swap space detected, skipping"
 else
     echo "no swap space detected, creating swapfile"
-    /bin/dd if=/dev/zero of="$PATH" bs=1M count="$MB"
-    /sbin/mkswap "$PATH"
-    /sbin/swapon "$PATH"
+    /bin/dd if=/dev/zero of="$swap_path" bs=1M count="$mb"
+    /sbin/mkswap "$swap_path"
+    /sbin/swapon "$swap_path"
 fi
 
