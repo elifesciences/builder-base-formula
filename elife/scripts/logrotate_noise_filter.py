5d8992d804ab63b2554252725f36bfd18ba96a87
Luke <lsh-0@users.noreply.github.com>
2021-05-03 12:26:57 +0930

    logrotate, added extra error to look for when ignoring noise

--- elife/scripts/logrotate_noise_filter.py
+++ elife/scripts/logrotate_noise_filter.py
@@ -1,7 +1,8 @@
-# filters out noise in logrotate in Ubuntu 18.04
-# https://github.com/logrotate/logrotate/issues/169
-# if this can be done with an elaborate grep or sed or awk or
-# whatever, feel free to replace it.
+"""filters out noise in logrotate in Ubuntu 18.04:
+  https://github.com/logrotate/logrotate/issues/169
+if this can be done with an elaborate grep or sed or 
+awk etc, feel free to replace it."""
+import sys
 import fileinput
 buffer=[]
 for line in fileinput.input():
@@ -9,12 +10,12 @@ for line in fileinput.input():
         buffer.append(line)
         continue
     if buffer \
-       and line.startswith('switching euid to') \
+       and (line.startswith('switching euid to') or line.startswith('switching uid to')) \
        and buffer[-1].startswith('error: Compressing program wrote following message to stderr'):
         buffer.pop() # remove last entry
 # if errors remain after filtering the noise out, exit successfully
 # this mimics current grep behaviour on matching on an error
 if buffer:
-    exit(0)
+    sys.exit(0)
 # no errors, die. the 'logger' won't be called, errors won't be reported
-exit(1)
+sys.exit(1)
