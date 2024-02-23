975b34fbcf7b2782b868c4b4425978041883c1d8
Luke <2142748+lsh-0@users.noreply.github.com>
2024-02-23 14:34:46 +1030

    fastly-ip-ranges.sh, added description to script

--- elife/scripts/fastly-ip-ranges.sh
+++ elife/scripts/fastly-ip-ranges.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# downloads Fastly's IP addresses so Caddy can trust them. 
+# Trusing these IPs means Caddy will set the correct forwarded headers on requests.
 set -eu -o pipefail
 rm -f /tmp/fastly-ip-ranges
 curl --silent "https://api.fastly.com/public-ip-list" | jq -r '.[][]' | sed -z 's/\n/ /g' > /tmp/fastly-ip-ranges
